ac22cd0d5233d27bc0e45acc7882d4bd2a0a68f0
AlexChen <alex.chen20181028@gmail.com>
2025-01-03 21:03:34 +0800

    HW1

---
+++ HW1/hw1-3.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+
+typedef struct MinHeap {
+    int size;
+    int data[MAX_SIZE];
+} MinHeap;
+
+//swap two elements
+void swap(int *a, int *b) {
+    int temp = *a;//get a's value
+    *a = *b;//give b's value to a 
+    *b = temp;//give a's value to a
+}
+
+//insert a new element into the Min-Heap
+void insert(MinHeap *heap, int value) {
+    if (heap->size >= MAX_SIZE) {
+        printf("Heap overflow\n");
+        return;
+    }
+
+    //insert the new element at the end of the heap
+    heap->data[heap->size] = value;
+    int current = heap->size;//the number of data
+    heap->size++;
+
+    //heapify up
+    while (current > 0) {
+        int parent = (current - 1) / 2;
+        if (heap->data[current] < heap->data[parent]) {
+            swap(&heap->data[current], &heap->data[parent]);
+            current = parent;
+        } else {
+            break;
+        }
+    }
+}
+
+//heapify down from a given index
+void heapifyDown(MinHeap *heap, int index) {
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+    int smallest = index;
+
+    if (left < heap->size && heap->data[left] < heap->data[smallest]) {
+        smallest = left;
+    }
+
+    if (right < heap->size && heap->data[right] < heap->data[smallest]) {
+        smallest = right;
+    }
+
+    if (smallest != index) {
+        swap(&heap->data[index], &heap->data[smallest]);
+        heapifyDown(heap, smallest);//check until index doesn't need to change
+    }
+}
+
+// Heapify up from a given index
+void heapifyUp(MinHeap *heap, int index) {
+    while (index > 0) {
+        int parent = (index - 1) / 2;
+        if (heap->data[index] < heap->data[parent]) {
+            swap(&heap->data[index], &heap->data[parent]);
+            index = parent;
+        } else {
+            break;
+        }
+    }
+}
+
+//delete a specific element from the Min-Heap
+void deleteElement(MinHeap *heap, int value) {
+    int index = -1;
+
+    //find the index of the element to be deleted
+    for (int i = 0; i < heap->size; i++) {
+        if (heap->data[i] == value) {
+            index = i;
+            break;
+        }
+    }
+
+    //if element is not found, do nothing
+    if (index == -1) {
+        return;
+    }
+
+    //replace the element with the last element in the heap
+    heap->data[index] = heap->data[heap->size - 1];
+    heap->size--;
+
+    // Determine whether to heapify up or down
+    if (index > 0 && heap->data[index] < heap->data[(index - 1) / 2]) {
+        heapifyUp(heap, index); // Heapify up if the element is smaller than its parent
+    } else {
+        heapifyDown(heap, index); // Otherwise, heapify down
+    }
+}
+
+//print the heap in level-order
+void printHeap(MinHeap *heap) {
+    for (int i = 0; i < heap->size; i++) {
+        printf("%d ", heap->data[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    MinHeap heap;
+    heap.size = 0;
+
+    char command[10];
+    int value;
+
+    //read commands until the end of input
+    while (scanf("%s %d", command, &value) != EOF) {
+        if (command[0] == 'i') {//command is "insert"
+            insert(&heap, value);
+        } else if (command[0] == 'd') {//command is "delete"
+            deleteElement(&heap, value);
+        }
+    }
+
+    //print the final state of the heap in level-order
+    printHeap(&heap);
+
+    return 0;
+}
